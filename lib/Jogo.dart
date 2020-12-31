901233eeb870966bf1db21293f656b82f0e8b2f6
wandersontx <wandersonh89@gmail.com>
2020-12-31 15:50:44 -0300

    Interface

---
+++ lib/Jogo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  @override
+  _JogoState createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("JokenPo"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha do App",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Image.asset("images/padrao.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 32, bottom: 16),
+            child: Text(
+              "Escolha um opção abaixo",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset("images/pedra.png", height: 100),
+              Image.asset("images/papel.png", height: 100),
+              Image.asset("images/tesoura.png", height: 100),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
