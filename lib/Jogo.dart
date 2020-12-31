d019c9245035932aa02d2e93acf6d066c6c260e4
wandersontx <wandersonh89@gmail.com>
2020-12-31 17:20:05 -0300

    Logica do jogo

--- lib/Jogo.dart
+++ lib/Jogo.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Jogo extends StatefulWidget {
@@ -6,6 +8,9 @@ class Jogo extends StatefulWidget {
 }
 
 class _JogoState extends State<Jogo> {
+  var _imageApp = AssetImage("images/padrao.png");
+  var _messageJogo = "Escolha um opção abaixo";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,19 +28,11 @@ class _JogoState extends State<Jogo> {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-          GestureDetector(
-            onTap: () {
-              print("Imagem clicada!");
-            },
-            onDoubleTap: () {
-              print("Clicou duas vezes");
-            },
-            child: Image.asset("images/padrao.png"),
-          ),
+          Image(image: this._imageApp),
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
             child: Text(
-              "Escolha um opção abaixo",
+              _messageJogo,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
@@ -43,13 +40,42 @@ class _JogoState extends State<Jogo> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset("images/pedra.png", height: 100),
-              Image.asset("images/papel.png", height: 100),
-              Image.asset("images/tesoura.png", height: 100),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("pedra"),
+                child: Image.asset("images/pedra.png", height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("papel"),
+                child: Image.asset("images/papel.png", height: 100),
+              ),
+              GestureDetector(
+                onTap: () => _opcaoSelecionada("tesoura"),
+                child: Image.asset("images/tesoura.png", height: 100),
+              ),
             ],
           )
         ],
       ),
     );
+  }
+
+  void _opcaoSelecionada(String escolhaUsuario) {
+    var opcoes = ["pedra", "papel", "tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcoes[numero];
+    var msgJogo = "Você perdeu :(";
+
+    if ((escolhaUsuario == 'pedra' && escolhaApp == 'tesoura') ||
+        (escolhaUsuario == 'tesoura' && escolhaApp == 'papel') ||
+        (escolhaUsuario == 'papel' && escolhaApp == 'pedra')) {
+      msgJogo = "Você ganhou :)";
+    } else if (escolhaUsuario == escolhaApp) {
+      msgJogo = "Empate :-)";
+    }
+
+    setState(() {
+      this._imageApp = AssetImage("images/" + escolhaApp + ".png");
+      this._messageJogo = msgJogo;
+    });
   }
 }
